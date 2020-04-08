6698d728da03d486180ce5aa0d9e34cfb651c3af
YeoEunseong <e7838752@gmail.com>
2020-04-08 21:21:54 +0900

    05.함수 과제

---
+++ 05.함수/timer1.js
@@ -0,0 +1,12 @@
+/*1초 간격으로 현재 시각을 출력하는 코드를 구현하시오.
+10번만 반복 호출하고 종료 되도록(clearInterval) 구현하시오.
+힌트: 호출 횟수는 전역 변수로 세어야 함.*/
+
+
+function printTime() {
+    if(index++>=3) clearInterval(id); 
+    console.log("현재시간: " + new Date());
+  }
+
+let index=1;
+let id = setInterval(printTime, 1000);
