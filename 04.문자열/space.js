6a66520b8e97b819b92ac6dcc5bc4c1381f36c1b
YeoEunseong <e7838752@gmail.com>
2020-04-08 21:21:05 +0900

    04.문자열 과제

---
+++ 04.문자열/space.js
@@ -0,0 +1,7 @@
+/*"When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."
+위 문자열에서 공백을 모두 제거한 새 문자열을 만들고 그 문자열을 출력하시오.*/
+
+let str = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
+let str2 = str.replace(/ /g,"");
+
+console.log(str2);
